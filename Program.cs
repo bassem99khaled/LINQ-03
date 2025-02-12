134f98507a105fa44ef6e72d7e90d4780df08ef5
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-02-12 14:48:38 +0200

    LINQ – Grouping Operators

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
-﻿namespace LINQ_03
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text.RegularExpressions;
+
+namespace LINQ_03
 {
     internal class Program
     {
@@ -47,17 +55,64 @@
                 {
                     Console.WriteLine($"  {product.ProductName} (Stock: {product.UnitsInStock})");
                 }
-            } 
-                #endregion
-
-
-
-                #region
-
-
-                #endregion
-
-
             }
+            #endregion
+
+
+
+            #region  LINQ – Grouping Operators
+
+       
+                // 1. Group numbers by their remainder when divided by 5
+
+                List<int> numbers = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+                var numberGroups = numbers.GroupBy(n => n % 5);
+
+                
+                foreach (var group in numberGroups)
+                {
+                    Console.WriteLine($"Remainder {group.Key}:");
+                    foreach (var number in group)
+                    {
+                        Console.WriteLine(number);
+                    }
+                }
+
+         
+
+                // 2. Group words by their first letter using dictionary_english.txt
+
+                var words = File.ReadAllLines("dictionary_english.txt").Where(w => !string.IsNullOrWhiteSpace(w));
+                var wordGroups = words.GroupBy(w => w[0].ToString().ToUpper());
+
+                
+
+                foreach (var group in wordGroups)
+                {
+                    Console.WriteLine($"Letter {group.Key}:");
+                    foreach (var word in group.Take(5)) 
+                    {
+                        Console.WriteLine(word);
+                    }
+                }
+
+         
+
+                // 3. Group words with same characters together
+
+                string[] arr = { "from", "salt", "earn", " last", "near", "form" };
+                var Groups = arr.GroupBy(w => new string(w.Trim().ToCharArray().OrderBy(c => c).ToArray()));
+
+           
+                foreach (var group in Groups)
+                {
+                    Console.WriteLine($"Group: {string.Join(", ", group)}");
+                }
+       
+
+    #endregion
+
+
+}
     }
 }
